06af3cad37216fc972986bd227b22698d4664ef7
weslen02 <weslen-a@hotmail.com>
2019-04-02 18:00:30 -0300

    exercise update

---
+++ Exercicios/HoraDoSistema/src/com/cursoemvideotreinamento/HoraDosSistema.java
@@ -0,0 +1,12 @@
+package com.cursoemvideotreinamento;
+import java.util.Date;
+
+public class HoraDosSistema  {
+
+    public static void main(String[] args) {
+	// write your code here
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é = " + relogio);
+        System.out.println(relogio.toString());
+    }
+}
---
+++ Exercicios/IdiomaSistema/src/com/cursoemvideotreinamento/Main.java
@@ -0,0 +1,8 @@
+package com.cursoemvideotreinamento;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
